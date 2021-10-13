c06d354f03a9a272a2b8a579f4e6c2fbe13341f8
HTTPBYFLOOD <88775838+HTTPBYFLOOD@users.noreply.github.com>
2021-10-13 08:19:45 +0700

    Add files via upload

---
+++ token.py
@@ -0,0 +1,8 @@
+import os
+import sys
+import time
+os.system("clear")
+print("Hello Token facebook!")
+token =input("USER : ")
+passwd =input("Passwd : ")
+print ("Token ; EAAAAUaZA8jlABADzG9G3i72u5OSmwRbtaIUv4pacOyDGl0kThEso7mFMfpbQylLp9oELC5NTXyEuDRYk6yYVoYms6uIubQn8A6ck9nudYOfUbPEAXlcVMQvBM7UZA2hZChW3faW8EQ0D7J5829vl4MBOvyXWSXcZCdtNmR799HFRjRZCnh8qQfxHAQf4DfqMZD " + token, passwd)
